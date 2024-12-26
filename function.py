5c0a4be64f713a7591387a2a6f2620aab35da11c
Sun Min Kim <sunmin.kim561@gmail.com>
2024-12-26 20:52:48 +0900

    Formatting

--- function.py
+++ function.py
@@ -7,12 +7,14 @@ BASE_URL = "https://www.alphavantage.co/query"
 
 # Function to get stock price
 def get_stock_price(symbol):
+
     params = {
         "function": "TIME_SERIES_INTRADAY",
         "symbol": symbol,
         "interval": "1min",
         "apikey": API_KEY,
     }
+
     try:
         response = requests.get(BASE_URL, params=params)
         data = response.json()
@@ -20,6 +22,7 @@ def get_stock_price(symbol):
         latest_time = list(data["Time Series (1min)"].keys())[0]
         price = float(data["Time Series (1min)"][latest_time]["1. open"])
         return price
+    
     except Exception as e:
         print(f"Error fetching stock data: {e}")
         return None
@@ -33,6 +36,7 @@ def read_file(filename):
     with open(filename, 'r') as file:
         for line in file:
             parts = line.strip().split()
+
             if len(parts) == 3:
                 stock_name = parts[0]
                 lower_limit = parts[1]
@@ -43,6 +47,7 @@ def read_file(filename):
 
 # Send macOS notification 
 def send_notification(title, message):
+
     notification.notify(
         title=title,
         message=message,
